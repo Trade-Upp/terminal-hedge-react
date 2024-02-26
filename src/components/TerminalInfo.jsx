3fb23bea37613e5155b7b31c423cc090316f93cb
Vladyslav <savelyevlad@gmail.com>
2024-02-26 09:28:47 +0100

    added buttons and entries

--- src/components/TerminalInfo.jsx
+++ src/components/TerminalInfo.jsx
@@ -72,9 +72,10 @@ export default function TerminalInfo({ symbol, apiKey, apiSecret, testnet, clien
         return
       }
       result.push({
-        size: position.positionAmt,
+        size: parseFloat(position.positionAmt) * parseFloat(position.leverage),
+        positionAmt: position.positionAmt,
         entryPrice: position.entryPrice,
-        profit: position.unRealizedProfit,
+        profit: Math.round(parseFloat(position.unRealizedProfit) * 1e2) / 1e2,
         liquidation: position.liquidationPrice,
         margin: position.isolated ? 'isolated' : 'cross',
         positionSide: position.positionSide,
@@ -87,8 +88,10 @@ export default function TerminalInfo({ symbol, apiKey, apiSecret, testnet, clien
     positions: []
   })
 
+  const thClasses = "px-2 py-3"
+
   function getSizeClass(positionSide) {
-    let result = "px-6 py-4 "
+    let result = thClasses + " "
     if (positionSide == 'LONG') {
       result += "text-green-700"
     }
@@ -99,7 +102,7 @@ export default function TerminalInfo({ symbol, apiKey, apiSecret, testnet, clien
   }
 
   function getProfitClass(profit) {
-    let result = "px-6 py-4 "
+    let result = thClasses + " "
     if (profit > 0) {
       result += "text-green-700"
     }
@@ -112,23 +115,32 @@ export default function TerminalInfo({ symbol, apiKey, apiSecret, testnet, clien
   return (
     <>
       <div className="overflow-x-auto p-2">
-        <table className="thin-scrollbar w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs uppercase text-gray-400 bg-zinc-900">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className={thClasses}>
                 Size
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className={thClasses}>
                 Entry price
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className={thClasses}>
                 Profit
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className={thClasses}>
                 Liquidation
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className={thClasses}>
                 Margin
+              </th>
+              <th scope="col" colSpan={2} className={thClasses + " text-center"}>
+                Close position
+              </th>
+              <th scope="col" className={thClasses}>
+                Price
+              </th>
+              <th scope="col" className={thClasses}>
+                Size
               </th>
             </tr>
           </thead>
@@ -139,19 +151,33 @@ export default function TerminalInfo({ symbol, apiKey, apiSecret, testnet, clien
               return (
                 <tr key={position.positionSide} className="border-b border-zinc-700">
                   <th scope="row" className={sizeClass}>
+                    {/* TODO: size is wrong */}
                     {position.size}
                   </th>
-                  <td className="px-6 py-4">
+                  <td className={thClasses}>
                     {position.entryPrice}
                   </td>
                   <td className={profitClass}>
                     {position.profit}
                   </td>
-                  <td className="px-6 py-4">
+                  <td className={thClasses}>
                     {position.liquidation}
                   </td>
-                  <td className="px-6 py-4">
+                  <td className={thClasses}>
                     {position.margin}
+                  </td>
+                  {/* TODO: copy functionality from python terminal */}
+                  <td className={thClasses}>
+                    <button>Market</button>
+                  </td>
+                  <td className={thClasses}>
+                    <button>Limit</button>
+                  </td>
+                  <td className={thClasses}>
+                    <input className="w-20" value={position.entryPrice} />
+                  </td>
+                  <td className={thClasses}>
+                    <input className="w-20" value={position.positionAmt} />
                   </td>
                 </tr>
               )
