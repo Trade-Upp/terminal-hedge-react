48f8f838af88364b6646fac763ab414a78f46de1
Vladyslav <savelyevlad@gmail.com>
2024-03-13 20:17:40 +0100

    notification components

---
+++ src/components/notifications/WarningNotification.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons"
+import NotificationProgress from './NotificationProgress'
+
+
+export default function WarningNotification({ children, progress, onClick }) {
+
+  const icon = <FontAwesomeIcon icon={faCircleExclamation} className="mr-2" />
+
+  return (
+    <>
+      <div className='m-4 pointer-events-auto select-none' onClick={onClick}>
+        <div className='rounded bg-yellow-700 opacity-80 text-slate-50 h-12'>
+          <NotificationProgress
+            icon={icon}
+            progress={progress}
+            progressColor={'bg-yellow-600'}>
+            {children}
+          </NotificationProgress>
+        </div>
+      </div>
+    </>
+  )
+}
